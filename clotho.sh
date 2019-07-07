74249c82de9987a98798fcf1bf2ef7703fc631b9
kiarahmani <rahmank@purdue.edu>
2019-07-07 18:22:32 -0400

    edits

--- clotho.sh
+++ clotho.sh
@@ -1,1 +1,66 @@
-echo "running clotho"
+#!/bin/bash
+# ------------------------------------------------------------------
+# [Kia Rahmani] CLOTHO
+#          	Directed Test Generation for Weakly Consistent Database Systems 
+# ------------------------------------------------------------------
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
+# ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# ------------------------------------------------------------------
+
+
+
+VERSION=1.0.0
+USAGE="Usage: command -ihv args"
+
+# --- Options processing -------------------------------------------
+if [ $# == 0 ] ; then
+    echo $USAGE
+    exit 1;
+fi
+
+while getopts ":i:vh" optname
+  do
+    case "$optname" in
+      "v")
+        echo "Version $VERSION"
+        exit 0;
+        ;;
+      "a")
+        echo "-i argument: $OPTARG"
+        ;;
+      "v")
+        echo "-i argument: $OPTARG"
+        ;;
+      "h")
+        echo $USAGE
+        exit 0;
+        ;;
+      "?")
+        echo "Unknown option $OPTARG"
+        exit 0;
+        ;;
+      ":")
+        echo "No argument value for option $OPTARG"
+        exit 0;
+        ;;
+      *)
+        echo "Unknown error while processing options"
+        exit 0;
+        ;;
+    esac
+  done
+
+shift $(($OPTIND - 1))
+
+param1=$1
+param2=$2
+
+
+# --- Body --------------------------------------------------------
+#  SCRIPT LOGIC GOES HERE
+echo $param1
+echo $param2
+# -----------------------------------------------------------------
---
+++ scripts/setup.sh
@@ -0,0 +1,4 @@
+# TODO: are hard-coded for system under test - must be generalized
+export CLOTHO_RT_PATH=/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/rt.jar
+export CLOTHO_JCE_PATH=/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/jce.jar
+
