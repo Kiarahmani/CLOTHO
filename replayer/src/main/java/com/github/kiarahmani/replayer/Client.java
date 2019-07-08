5d2d18c2f8d2df9347de01311ce5a5c7f2d107b0
kiarahmani <rahmank@purdue.edu>
2019-07-08 16:41:05 -0400

    ready for documentation

--- replayer/src/main/java/com/github/kiarahmani/replayer/Client.java
+++ replayer/src/main/java/com/github/kiarahmani/replayer/Client.java
@@ -44,27 +44,30 @@ public class Client {
 			e.printStackTrace();
 		}
 	}
-
-	public void one_read(Long key) throws SQLException {
+	public void two_reads(Long key) throws SQLException {
 		PreparedStatement stmt = connect.prepareStatement("SELECT value " + "FROM " + "ACCOUNTS" + " WHERE id = ?");
 		stmt.setLong(1, key);
 		ResultSet rs = stmt.executeQuery();
 		rs.next();
-		int read_val = rs.getInt("VALUE");
-		assert (read_val == 100);
-		System.out.println(read_val);
-	}
+		Long read_val1 = rs.getLong("VALUE");
+		System.out.println(read_val1);
 
-	public void two_writes(Long key) throws SQLException {
+		PreparedStatement stmt2 = connect.prepareStatement("SELECT value " + "  FROM " + "ACCOUNTS" + " WHERE id = ?");
+		stmt2.setLong(1, key);
+		ResultSet rs2 = stmt2.executeQuery();
+		rs2.next();
+		Long read_val2 = rs2.getLong("VALUE");
+		System.out.println(read_val2);
+		assert (read_val1 == read_val2);
+	}
+	
+	
+	
+	public void one_write(Long key, Long new_val) throws SQLException {
 		PreparedStatement stmt1 = connect.prepareStatement("UPDATE ACCOUNTS SET value = ?" + " WHERE id = ?");
-		stmt1.setLong(1, 50);
+		stmt1.setLong(1, new_val);
 		stmt1.setLong(2, key);
 		stmt1.executeUpdate();
-
-		PreparedStatement stmt2 = connect.prepareStatement("UPDATE ACCOUNTS  SET value = ?" + " WHERE id = ?");
-		stmt2.setLong(1, 100);
-		stmt2.setLong(2, key);
-		stmt2.executeUpdate();
 	}
 
 }
